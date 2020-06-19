Fix rotation issue

facenet.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import numpy as np
 from scipy import misc
 from sklearn.model_selection import KFold
-from skimage.transform import rotate
+from scipy import ndimage
 from scipy import interpolate
 from tensorflow.python.training import training
 import random
@@ -16,7 +16,7 @@ import re
 from tensorflow.python.platform import gfile
 import math
 from six import iteritems
-
+import matplotlib.pyplot as plt
 
 def triplet_loss(anchor, positive, negative, alpha):
     """Calculate the triplet loss according to the FaceNet paper
@@ -70,7 +70,8 @@ def shuffle_examples(image_paths, labels):
 # Rotate the image using a random angle in the interval [-10, +10] degrees
 def rotate_image(image):
     angle = np.random.uniform(low=-10.0, high=10.0)
-    return rotate(image, angle)
+    image_rotated = ndimage.rotate(image, angle, reshape=False)
+    return image_rotated
   
 # 1: Random rotate 2: Random crop  4: Random flip  8:  Fixed image standardization  16: Flip
 RANDOM_ROTATE = 1

train_softmax.py
@@ -95,7 +95,7 @@ def main(args):
         index_queue = tf.train.range_input_producer(range_size, num_epochs=None,
                              shuffle=True, seed=None, capacity=32)
 
-        index_dequeue_op = index_queue.dequeue_many(args.batch_size*args.epoch_size, 'index_dequeue')
+        index_dequeue_op = index_queue.dequeue_many(args.batch_size * args.epoch_size, 'index_dequeue')
         
         learning_rate_placeholder = tf.placeholder(tf.float32, name='learning_rate')
         batch_size_placeholder = tf.placeholder(tf.int32, name='batch_size')
@@ -226,7 +226,7 @@ def main(args):
                 if len(val_image_list)>0 and ((epoch-1) % args.validate_every_n_epochs == args.validate_every_n_epochs-1 or epoch==args.max_nrof_epochs):
                     validate(args, sess, epoch, val_image_list, val_label_list, enqueue_op, image_paths_placeholder, labels_placeholder, control_placeholder,
                         phase_train_placeholder, batch_size_placeholder, 
-                        stat, total_loss, regularization_losses, cross_entropy_mean, accuracy, args.validate_every_n_epochs, args.use_fixed_image_standardization)
+                        stat, total_loss, regularization_losses, cross_entropy_mean, accuracy, args.validate_every_n_epochs, p_fixed_img_std)
                 stat['time_validate'][epoch-1] = time.time() - t
 
                 # Save variables and the metagraph if it doesn't exist already
@@ -237,7 +237,7 @@ def main(args):
                 if args.lfw_dir:
                     evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder, phase_train_placeholder, batch_size_placeholder, control_placeholder, 
                         embeddings, label_batch, lfw_paths, actual_issame, args.lfw_batch_size, args.lfw_nrof_folds, log_folder, step, summary_writer, stat, epoch, 
-                        args.lfw_distance_metric, args.lfw_subtract_mean, args.lfw_use_flipped_images, args.use_fixed_image_standardization)
+                        args.lfw_distance_metric, args.lfw_subtract_mean, args.lfw_use_flipped_images, p_fixed_img_std)
                 stat['time_evaluate'][epoch-1] = time.time() - t
 
                 print('Saving statistics')